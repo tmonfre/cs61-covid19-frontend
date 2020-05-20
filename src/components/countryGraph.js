cbae795a0f912ec0fbd2a7b4b64a97c0dd98da4e
Emma Rafkin <emmarafkin@gmail.com>
2020-05-20 16:57:32 -0400

    state data working

--- src/components/countryGraph.js
+++ src/components/countryGraph.js
@@ -5,57 +5,57 @@ import { getCountry, getState } from '../state/actions';
 
 
 class CountryGraph extends React.Component {
-    d = {
-      labels: [],
-      datasets: [
-        {
-          label: 'Cases',
-          fill: false,
-          lineTension: 0.5,
-          backgroundColor: 'rgba(75,192,192,1)',
-          borderColor: 'rgba(0,0,0,1)',
-          borderWidth: 2,
-          data: [],
-        },
-        {
-          label: 'Deaths',
-          fill: false,
-          lineTension: 0.5,
-          backgroundColor: 'rgba(75,192,192,1)',
-          borderColor: 'rgba(0,0,255,1)',
-          borderWidth: 2,
-          data: [],
-        },
-      ],
-    };
-
-    componentDidMount() {
-      if (this.props.type === 'country') {
-        this.props.getCountry();
-      } else if (this.props.type === 'state') {
-        this.props.getState(this.props.state);
-      }
+  componentDidMount() {
+    if (this.props.type === 'country') {
+      this.props.getCountry();
+    } else if (this.props.type === 'state') {
+      this.props.getState(this.props.state);
     }
+  }
 
-    handleData = () => {
+    handleData = (d) => {
       console.log(this.props.type);
       const data = this.props.type === 'country' ? this.props.countryData : this.props.stateData;
+      console.log(this.props.countryData);
       console.log(data);
       data.forEach((day) => {
-        this.d.labels.push(JSON.stringify(new Date(day.Date).toDateString()));
-        this.d.datasets[0].data.push(parseInt(day.CaseCountSum, 10));
-        this.d.datasets[1].data.push(parseInt(day.DeathCountSum, 10));
+        d.labels.push(JSON.stringify(new Date(day.Date).toDateString()));
+        d.datasets[0].data.push(parseInt(day.CaseCountSum, 10));
+        d.datasets[1].data.push(parseInt(day.DeathCountSum, 10));
       });
     }
 
 
     render() {
-      console.log(this.d);
-      this.handleData();
+      const d = {
+        labels: [],
+        datasets: [
+          {
+            label: 'Cases',
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 2,
+            data: [],
+          },
+          {
+            label: 'Deaths',
+            fill: false,
+            lineTension: 0.5,
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,255,1)',
+            borderWidth: 2,
+            data: [],
+          },
+        ],
+      };
+      console.log(d);
+      this.handleData(d);
       return (
         <div id="countrygraph">
           <Line
-            data={this.d}
+            data={d}
             options={{
               title: {
                 display: true,
