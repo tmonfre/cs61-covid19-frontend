f324e9c74aa80cfd703aa1f22f3a4b9e71499a66
Anne Bailey <anne.g.bailey.22@dartmouth.edu>
2020-05-22 14:47:34 -0400

    basic toggle between map and chart set up

--- src/components/home.js
+++ src/components/home.js
@@ -1,13 +1,15 @@
+/* eslint-disable react/button-has-type */
 /* eslint-disable no-param-reassign */
 import React from 'react';
 import { connect } from 'react-redux';
 import { withRouter } from 'react-router-dom';
 import { Fade } from 'react-reveal';
 import * as am4core from '@amcharts/amcharts4/core';
-import * as am4maps from '@amcharts/amcharts4/maps';
-import am4geodataUSLow from '@amcharts/amcharts4-geodata/usaTerritories2Low';
+// import * as am4maps from '@amcharts/amcharts4/maps';
+// import am4geodataUSLow from '@amcharts/amcharts4-geodata/usaTerritories2Low';
 import am4themesAnimated from '@amcharts/amcharts4/themes/animated';
 import am4themesDataVis from '@amcharts/amcharts4/themes/dataviz';
+import Country from './country';
 import State from './state';
 import Graph from './graph';
 
@@ -16,103 +18,36 @@ import '../styles/home.scss';
 am4core.useTheme(am4themesDataVis);
 am4core.useTheme(am4themesAnimated);
 
-const stateAbbreviations = require('../data/state-abbreviations.json');
+// const stateAbbreviations = require('../data/state-abbreviations.json');
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      mountedGraph: false,
+      showMap: true,
     };
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (Object.keys(nextProps.stateData).length > 0 && !this.state.mountedGraph) {
-      const stateData = [];
-      Object.keys(nextProps.stateData).forEach((stateName) => {
-        stateData.push({
-          ...nextProps.stateData[stateName],
-          id: stateAbbreviations[stateName],
-          value: nextProps.stateData[stateName].caseCountSum,
-        });
-      });
-
-      // create map of US states and territories
-      const map = am4core.create('chartdiv', am4maps.MapChart);
-      map.geodata = am4geodataUSLow;
-      map.projection = new am4maps.projections.Miller();
-      map.chartContainer.wheelable = false;
-
-      // define state shape and data
-      const polygonSeries = map.series.push(new am4maps.MapPolygonSeries());
-      polygonSeries.useGeodata = true;
-      polygonSeries.data = stateData;
-
-      // set heat map fill rules
-      polygonSeries.heatRules.push({
-        property: 'fill',
-        target: polygonSeries.mapPolygons.template,
-        min: map.colors.getIndex(1).brighten(1),
-        max: map.colors.getIndex(1).brighten(-0.3),
-      });
-
-      // set up heat legend
-      const heatLegend = map.createChild(am4maps.HeatLegend);
-      heatLegend.series = polygonSeries;
-      heatLegend.align = 'right';
-      heatLegend.valign = 'bottom';
-      heatLegend.width = am4core.percent(20);
-      heatLegend.marginRight = am4core.percent(6);
-      heatLegend.minValue = 0;
-      heatLegend.maxValue = 30000;
-
-      // configure series tooltip
-      const polygonTemplate = polygonSeries.mapPolygons.template;
-      polygonTemplate.tooltipText = '{name}\n{caseCountSum} Cases\n{deathCountSum} Deaths';
-      polygonTemplate.nonScalingStroke = true;
-      polygonTemplate.strokeWidth = 0.5;
-
-      // Create hover state and set alternative fill color
-      const hs = polygonTemplate.states.create('hover');
-      hs.properties.fill = am4core.color('#c2c2c2');
-
-      map.tooltip.getFillFromObject = false;
-      map.tooltip.background.fill = am4core.color('#c2c2c2');
-
-      // set state click handler
-      polygonTemplate.events.on('hit', this.handleStateClick);
-
-      // save map
-      map.zoomControl = new am4maps.ZoomControl();
-      this.map = map;
-
-      this.setState({
-        mountedGraph: true,
-      });
-    }
+  handleShowMap = () => {
+    this.setState({ showMap: true });
   }
 
-  componentWillUnmount() {
-    if (this.map) {
-      this.map.dispose();
-    }
+  handleShowChart = () => {
+    this.setState({ showMap: false });
   }
-
-  handleStateClick = (ev) => {
-    // ev.target.series.chart.zoomToMapObject(ev.target, 5);
-    console.log(`onClick: ${ev.target.dataItem.dataContext.name}`);
-    this.props.history.push(`/state/${ev.target.dataItem.dataContext.name}`);
-  }
-
 
   render() {
-    console.log(`in render: ${this.props.stateData}`);
     return (
       <Fade>
         <div id="home">
-          <div id="chartdiv" />
-          <Graph type="country" />
+          <button onClick={this.handleShowMap}>Show Map</button>
+          <button onClick={this.handleShowChart}>Show Chart</button>
+          {
+            this.state.showMap
+              ? <Country />
+              : <Graph type="country" />
+          }
           <State statename={this.props.match.params.statename} />
         </div>
       </Fade>
